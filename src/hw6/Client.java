34993060cf64025b462b130d682d07cd3246f0f8
Andrew <shpuntik0902@gmail.com>
2021-01-25 21:55:00 +0300

    hw6

--- src/hw6/Client.java
+++ src/hw6/Client.java
@@ -1,27 +1,24 @@
 package hw6;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
-import java.util.UUID;
-import com.google.gson.*;
+
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        String id = UUID.randomUUID().toString();
-        Socket socket = new Socket("localhost",8081);
-        Scanner scanner = new Scanner(System.in);
-        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-        String inputString;
-        do {
-            inputString = scanner.nextLine();
-            Message message = new Message();
-            message.setMessage(inputString);
-            message.setClientId(id);
-            dataOutputStream.writeUTF(new Gson().toJson(message));
-            dataOutputStream.flush();
-        } while (!"\\finish".equals(inputString));
-        socket.close();
+        try (Socket clientSocket = new Socket("localhost",8081)) {
+                DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+                new MessageReaderThread(dataInputStream).start();
+                Scanner scanner = new Scanner(System.in);
+                do {
+                    dataOutputStream.writeUTF(scanner.nextLine());
+                } while (true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
